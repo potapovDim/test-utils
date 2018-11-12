a450da9205b0861d11b878add50295d366717015
potapovDim <potapovd3012@gmail.com>
2018-11-12 13:02:34 +0200

    fix

---
+++ async-queue/promiseQueue.js
@@ -0,0 +1,82 @@
+const toPromisItems = (sleep, value) => new Promise((resolve) => {
+  setTimeout(() => resolve(value), sleep)
+})
+
+class Queue {
+  constructor() {
+    this.dataItem = null
+    this.queueItems = []
+  }
+
+  get data() {
+    return this.dataItem
+  }
+
+  set data(item) {
+    this.dataItem = item
+  }
+
+  wrapInQueue(f) {
+    this.queueItems.push(f)
+  }
+
+  clearQueue() {
+    this.queueItems = []
+  }
+
+  runQueu() {
+    return this.queueItems.reduce((accResolver, queueItem) => {
+      return accResolver.then((value) => {
+        this.data = value
+        // console.log(value)
+        return queueItem()
+      })
+    }, Promise.resolve(null))
+  }
+}
+
+class WorkeItem {
+  constructor() {
+    this.queue = new Queue()
+  }
+
+  execute() {
+    this.queue.runQueu()
+    this.queue.clearQueue()
+
+  }
+
+  data(cb) {
+    this.queue.wrapInQueue(() => cb(this.queue.data))
+
+    return this
+  }
+
+  functionFirst() {
+    this.queue.wrapInQueue(() => toPromisItems(1500, 'test 1'))
+    return this
+  }
+  functionSecont() {
+    this.queue.wrapInQueue(() => toPromisItems(800, 'test 2'))
+    return this
+  }
+  functionThird() {
+    this.queue.wrapInQueue(() => toPromisItems(800, 'test 3'))
+    return this
+  }
+  functionFourth() {
+    this.queue.wrapInQueue(() => toPromisItems(800, 'test 4'))
+    return this
+  }
+  functionFivth() {
+    this.queue.wrapInQueue(() => toPromisItems(800, 'test 5'))
+    return this
+  }
+}
+
+const item = new WorkeItem()
+
+item.functionFirst().functionFourth().data((value) => {
+  console.log(value, ' !!!!!!!!!!!!!!!!!!')
+}).functionThird().functionSecont().functionFirst().execute()
+

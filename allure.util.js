ddf166615e0eb97b63069d389308e648f2783d3d
potapovDim <potapovd3012@gmail.com>
2019-09-14 14:32:58 +0300

    find step

--- allure.util.js
+++ allure.util.js
@@ -17,3 +17,25 @@ function allureCleanUp() {
     }
   })
 }
+
+async function findAllSteps(namePart) {
+  let logins = 0
+  async function sleep(ms = 5000) {
+    return new Promise((res) => setTimeout(res, ms))
+  }
+  const runnedCases = document.querySelectorAll('.node__title')
+  for(let i = 0; i < runnedCases.length; i++) {
+    runnedCases[i].click()
+    await sleep(100)
+
+    const steps = document.querySelectorAll('.step__name')
+
+    for(let i = 0; i < steps.length; i++) {
+      if(steps[i].innerText.includes(namePart)) {
+        console.log(steps[i].innerText)
+        logins++
+      }
+    }
+  }
+  return logins
+}

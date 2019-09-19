d229e0dec5503629fc8f5fe773374f02517a2282
potapovDim <potapovd3012@gmail.com>
2019-09-19 14:40:22 +0300

    Initial set up report server

--- report-server/ lib/routs.js
+++ report-server/ lib/routs.js
@@ -1,8 +1,27 @@
 const Router = require('koa-router')
 
+/**
+ * @storage
+ * @example storage
+ * {
+ *  // execution date
+ *  // date should have some standart format
+ *  date: {
+ *    // build description
+ *    'some build description': [
+ *      {
+ *        testCaseId: 'some test case id',
+ *        stackTraceError: 'some test case fail error'
+ *      }
+ *    ]
+ *  }
+ * }
+ */
+const storage = {
+
+}
 
 const router = new Router()
-
 
 router.get('*', (ctx) => {
   ctx.status = 200
@@ -10,7 +29,9 @@ router.get('*', (ctx) => {
   return ctx
 });
 
+
 router.post('/create-server', (ctx) => {
+
   console.log(ctx.request.body)
   ctx.status = 200
   ctx.body = 'OK'
---
+++ report-server/ lib/storage.js
@@ -0,0 +1,34 @@
+// storage should link
+function addNewBuildDescription(date, buildDescription, storage) {
+  // if storage does not contain date add new date to storage
+  if(!storage[date]) {
+    storage[date] = {}
+  }
+  // add new build description as a new empty array
+  storage[date][buildDescription] = []
+
+  // true as a success
+  return true
+}
+
+
+function addNewTestCaseToBuild(date, buildDescription, testCaseData, storage) {
+  if(!storage[date]) {
+    storage[date] = {}
+  }
+
+  if(!storage[date][buildDescription]) {
+    storage[date][buildDescription] = []
+  }
+
+  storage[date][buildDescription].push(testCaseData)
+
+  // true as a success
+
+  return true
+}
+
+
+function buildWeekReport() {
+
+}

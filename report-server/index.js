f67f279378b81326c31c491547b4eb9172739fc8
potapovDim <potapovd3012@gmail.com>
2019-09-27 14:18:37 +0300

    fix

--- report-server/index.js
+++ report-server/index.js
@@ -1,9 +1,6 @@
 const Koa = require('koa2')
-
 const bodyParser = require('koa-bodyparser')
 const cors = require('@koa/cors')
-const fs = require('fs')
-const path = require('path')
 
 const app = new Koa()
 const {router} = require('./lib-reporter/routs')
